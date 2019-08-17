e7d5f62aa18f1bb9249ba3806194ce57adec0245
kegoba <egobakelvin@gmail.com>
2019-08-17 10:01:22 +0100

    setting change

--- kelvineapp/settings.py
+++ kelvineapp/settings.py
@@ -107,17 +107,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+#STATIC_URL = '/static/'
+
+#STATICFILES_DIRS = (
+   # os.path.join(STATIC_ROOT, "staticfiles"),
     
-)
+#)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS =( os.path.join(STATIC_ROOT, 'css/'),
+                    #os.path.join(STATIC_ROOT, 'javascript/'),
+                    #os.path.join(STATIC_ROOT, 'images/')
+                  )
 
 WHITENOISE_ROOT = os.path.join(BASE_DIR, "STATIC", "ROOT")
