adfc2a59824e15dc6f76b72c75ccbaa7a9e974c7
kegoba <egobakelvin@gmail.com>
2019-08-17 16:35:51 +0100

    changes on setting.py

--- kelvineapp/settings.py
+++ kelvineapp/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,5 +119,5 @@ STATICFILES_DIRS = (
 )
 
 
-#WHITENOISE_ROOT = os.path.join(BASE_DIR, "static/css", )
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_ROOT = os.path.join(BASE_DIR, "static/css", )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
